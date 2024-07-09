c4b9c724fda6d610a9ea777226c61b8df65c4577
Paul Chheang <phalcheat@gmail.com>
2024-07-08 21:55:07 -0500

    First Http GET request with useEffect()

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  /*****************************************************************************
+   * Sending http GET request to get all available places from backend server.
+   *****************************************************************************/
+
+  // Use useEffect() to prevent unfinite loop
+  useEffect(() => {
+    // Send http request to backend using vanilla js method 'fetch()'
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        // using the standard json() method to extract data in response object
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
 
   return (
     <Places
