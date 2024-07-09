e7ae5d46870eae86de5eaf6970f75087fe4d5a91
Paul Chheang <phalcheat@gmail.com>
2024-07-08 22:37:27 -0500

    Handling State for Controlling Data Loading Status

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false); // status of data loading/fetching
+  const [availablePlaces, setAvailablePlaces] = useState([]); // fetched loaded data
 
   /*****************************************************************************
    * Sending http GET request to get all available places from backend server.
@@ -13,9 +14,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
   useEffect(() => {
     // Use async await in modern JavaScript instead of promise function form ().then()
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     // we must execute the async function inside of this useEffect()
@@ -26,6 +29,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
